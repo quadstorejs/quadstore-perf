f6422ab20493b6a4d0ca65cf9f9a76d2b87d63d8
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-05-28 23:05:09 +0200

    adds combination using rdf-stores and quadstore-comunica

--- src/query-graph.ts
+++ src/query-graph.ts
@@ -9,22 +9,19 @@ import { DataFactory } from 'rdf-data-factory';
 import { Engine as QuadstoreEngine } from 'quadstore-comunica';
 import { Bindings, Quad } from '@rdfjs/types';
 import { Store as N3Store } from 'n3';
+import { RdfStore } from 'rdf-stores';
 import { QueryEngine as ComunicaEngine } from '@comunica/query-sparql-rdfjs';
 import assert from 'node:assert';
 
 const dataFactory = new DataFactory();
 const qty = 200_000;
 
-const doWrites = async (storeFn: (quad: Quad) => any) => {
-  for (let i = 0; i < qty; i += 1) {
-    await storeFn(dataFactory.quad(
-      dataFactory.namedNode(`ex://s${i}`),
-      dataFactory.namedNode(`ex://p${i}`),
-      dataFactory.namedNode(`ex://o${i}`),
-      dataFactory.namedNode(`ex://g${i % 1000}`),
-    ));
-  }
-};
+const quads: Quad[] = new Array(qty).fill(true).map((_, i) => dataFactory.quad(
+  dataFactory.namedNode(`ex://s${i}`),
+  dataFactory.namedNode(`ex://p${i}`),
+  dataFactory.namedNode(`ex://o${i}`),
+  dataFactory.namedNode(`ex://g${i % 1000}`),
+));
 
 main(async () => {
 
@@ -35,7 +32,7 @@ main(async () => {
      await store.open();
     timeEnd('setup');
     time('writes');
-    await doWrites((quad) => store.put(quad));
+    await store.multiPut(quads);
     timeEnd('writes');
     let count = 0;
     time('query - setup');
@@ -68,7 +65,7 @@ main(async () => {
     const engine = new QuadstoreEngine(store as unknown as Quadstore);
     timeEnd('setup');
     time('writes');
-    await doWrites((quad) => store.addQuad(quad));
+    store.addQuads(quads);
     timeEnd('writes');
     let count = 0;
     time('query - setup');
@@ -102,7 +99,7 @@ main(async () => {
     await store.open();
     timeEnd('setup');
     time('writes');
-    await doWrites((quad) => store.put(quad));
+    await store.multiPut(quads);
     timeEnd('writes');
     let count = 0;
     time('query - setup');
@@ -138,11 +135,10 @@ main(async () => {
     const engine = new ComunicaEngine();
     timeEnd('setup');
     time('writes');
-    await doWrites((quad) => store.addQuad(quad));
+    store.addQuads(quads);
     timeEnd('writes');
     let count = 0;
     time('query - setup');
-    // const iterator = await quadstore_engine.queryBindings(`SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o . } }`);
     const iterator = await engine.queryBindings(`SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o . } }`, {
       sources: [store],
       destination: store,
@@ -167,6 +163,41 @@ main(async () => {
   });
   
   console.log('n3, comunica: ', n3store_comunica['time']['partials']['query - reads']['time']);
+  
+  const rdfstore_quadstorecomunica = await runTest(async (backend, du, time, timeEnd, info) => {
+    time('setup');
+    const store = RdfStore.createDefault();
+    const engine = new QuadstoreEngine(store as any);
+    timeEnd('setup');
+    time('writes');
+    quads.forEach(q => store.addQuad(q));
+    timeEnd('writes');
+    let count = 0;
+    time('query - setup');
+    const iterator = await engine.queryBindings(`SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o . } }`, {
+      sources: [store],
+      destination: store,
+    });
+    const onDataRest = (bindings: Bindings) => {
+      count++;
+    };
+    const onDataFirst = (bindings: Bindings) => {
+      onDataRest(bindings);
+      timeEnd('query - setup');
+      time('query - reads');
+      iterator.on('data', onDataRest);
+    };
+    iterator.once('data', onDataFirst);
+    iterator.on('error', (err) => {
+      console.error(err);
+    });
+    await waitForEvent(iterator, 'end');
+    const query_read_duration = timeEnd('query - reads');
+    info('quads_per_second', round((qty / query_read_duration) * 1000, 2));
+    info('quads read', count);
+  });
+  
+  console.log('rdf-store, quadstore-comunica: ', rdfstore_quadstorecomunica['time']['partials']['query - reads']['time']);
   
 });
 
